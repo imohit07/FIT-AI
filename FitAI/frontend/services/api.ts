2b56a5d5597c4e0a9b4c73187c662459adae853b
Mohit khandelwal <mohit.khandelwal.feb07@gmail.com>
2026-03-23 02:04:34 +0530

    updated url

--- FitAI/frontend/services/api.ts
+++ FitAI/frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://fit-ai-backend-zekt.onrender.com/"
+  baseURL: "https://fit-ai-backend-zekt.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
